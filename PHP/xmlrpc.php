04c1cfe83df8ec702c4f27b0f941f0d1b1b1b1bd
AnmolGathe <90272232+AnmolGathe@users.noreply.github.com>
2022-01-13 22:53:22 +0530

    Add files via upload

---
+++ PHP/xmlrpc.php
@@ -0,0 +1,15 @@
+This XML file does not appear to have any style information associated with it. The document tree is shown below.
+<rsd xmlns="http://archipelago.phrasewise.com/rsd" version="1.0">
+<service>
+<engineName>WordPress</engineName>
+<engineLink>https://wordpress.org/</engineLink>
+<homePageLink>https://demo2.chethemes.com/enter</homePageLink>
+<apis>
+<api name="WordPress" blogID="1" preferred="true" apiLink="https://demo2.chethemes.com/enter/xmlrpc.php"/>
+<api name="Movable Type" blogID="1" preferred="false" apiLink="https://demo2.chethemes.com/enter/xmlrpc.php"/>
+<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://demo2.chethemes.com/enter/xmlrpc.php"/>
+<api name="Blogger" blogID="1" preferred="false" apiLink="https://demo2.chethemes.com/enter/xmlrpc.php"/>
+<api name="WP-API" blogID="1" preferred="false" apiLink="https://demo2.chethemes.com/enter/wp-json/"/>
+</apis>
+</service>
+</rsd>
